558efaeee4b0ad0f87d530ff0aa903233a062086
jimm9 Tran <jimm9tran@gmail.com>
2025-02-25 22:06:00 +0700

    update

--- my_e_commerce/vite.config.js
+++ my_e_commerce/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  base: process.env.APP_URL + '/build/',
+  base: process.env.VITE_ASSET_URL || '/',
   plugins: [
     laravel({
       input: ['resources/css/app.css', 'resources/js/app.js'],
